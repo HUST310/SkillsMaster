4b5df5dfb56f3dd5f7ae32762a56a86ce7cced3b
Liu Mengxiang <76817488+l-m-x1@users.noreply.github.com>
2023-06-12 16:42:58 +0800

    Add files via upload

---
+++ src/main/java/com/hust310/SkillsMaster/SkillsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.hust310.SkillsMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust310/SkillsMaster/SkillsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust310.SkillsMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
