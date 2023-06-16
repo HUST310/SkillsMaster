3728477ddf16b85b9a60efc5e38321884e03e43d
lmx <3361040032@qq.com>
2023-06-16 15:35:39 +0800

    register username

---
+++ src/main/java/com/hust310/SkillsMaster/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.hust310.SkillsMaster.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hust310.SkillsMaster.domain.Test;
+import com.hust310.SkillsMaster.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @Autowired
+    private TestService testService;
+
+    @PostMapping("/getTest")
+    public Page<Test> getTest(Integer page) {
+        Page<Test> testPage = testService.page(new Page<Test>(page, 2));
+        return testPage;
+    }
+}
