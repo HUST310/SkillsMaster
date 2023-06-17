a2152aa763c3627a6d41dc7bb37502d85c9c470f
lmx <3361040032@qq.com>
2023-06-17 15:17:03 +0800

    modify redis

--- src/main/java/com/hust310/SkillsMaster/controller/UserController.java
+++ src/main/java/com/hust310/SkillsMaster/controller/UserController.java
@@ -3,12 +3,28 @@ package com.hust310.SkillsMaster.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hust310.SkillsMaster.domain.User;
 import com.hust310.SkillsMaster.service.UserService;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+import org.apache.commons.io.FilenameUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -17,8 +33,7 @@ public class UserController {
 
     @PostMapping("/login")
     public String login(HttpSession session, @RequestBody User user) {
-        User sqluser = userService.getOne(new QueryWrapper<User>().eq("account", user.getAccount()));
-
+        User sqluser = userService.getById(user.getAccount());
         if (sqluser == null) {
             return "account error";
         } else if (!user.getPassword().equals(sqluser.getPassword())) {
@@ -45,12 +60,31 @@ public class UserController {
 
     @GetMapping("/getUserInfo")
     public User getUserInfo(HttpSession session) {
+        session.setAttribute("uid", 3);
         Integer uid = (Integer) session.getAttribute("uid");
-        return userService.getOne(new QueryWrapper<User>().eq("account", uid));
+        return userService.getById(uid);
     }
 
     @PostMapping("/modifyUserInfo")
-    public String modifyUserInfo(MultipartFile file) {
+    public String modifyUserInfo(StandardMultipartHttpServletRequest request, HttpSession session) throws IOException {
+        User user = new User();
+        session.setAttribute("uid", 3);
+        user.setAccount((Integer) session.getAttribute("uid"));
+
+        user.setUsername(request.getParameter("username"));
+        user.setSignature(request.getParameter("signature"));
+        user.setGender(request.getParameter("gender"));
+//        user.setAvatar();
+        MultiValueMap<String, MultipartFile> multiFileMap = request.getMultiFileMap();
+        MultipartFile file = request.getMultiFileMap().get("avatar").get(0);
+        UUID.randomUUID().toString();
+        String fileName = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(file.getOriginalFilename());
+        File file1 = new File("src/main/resources/static/img/", fileName);
+        FileUtils.writeByteArrayToFile(file1, file.getBytes());
+
+        user.setAvatar("img/" + fileName);
+        userService.saveOrUpdate(user);
         return "success";
     }
+
 }
