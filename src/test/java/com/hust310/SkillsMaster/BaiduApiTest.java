4124b005fed3103de377989150a2fc7c1a68c3b9
Casey219 <2414996036@qq.com>
2023-06-17 10:12:49 +0800

    test Content Review

---
+++ src/test/java/com/hust310/SkillsMaster/BaiduApiTest.java
@@ -0,0 +1,30 @@
+package com.hust310.SkillsMaster;
+
+import com.baidu.aip.contentcensor.AipContentCensor;
+import com.baidu.aip.contentcensor.EImgType;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+public class BaiduApiTest {
+
+    public static final String APP_ID = "34889997";
+    public static final String API_KEY = "2od67QEHyfepWsXQvR0idplp";
+    public static final String SECRET_KEY = "KKdGfAqowXsyWgNdrGpnKNI8X2NfaFyj";
+
+    @Test
+    public void testText() {
+
+        AipContentCensor client=new AipContentCensor(APP_ID, API_KEY, SECRET_KEY);
+        String text="蔡英文";
+        JSONObject response = client.textCensorUserDefined(text);
+        System.out.println(response.toString());
+    }
+
+    @Test
+    public void testImage() {
+        AipContentCensor client=new AipContentCensor(APP_ID, API_KEY, SECRET_KEY);
+        String url="https://tse4-mm.cn.bing.net/th/id/OIP-C.kHinIF0bOKfFmAOxpibyfwAAAA?w=132&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7";
+        JSONObject response = client.imageCensorUserDefined(url, EImgType.URL, null);
+        System.out.println(response.toString());
+    }
+}
