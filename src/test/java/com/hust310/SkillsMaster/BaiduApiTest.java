c77e4e37ac78d670a5ab6ddbdeea2c9ee856e50b
Casey219 <2414996036@qq.com>
2023-06-18 10:26:44 +0800

    change baiduAPI implementation method

--- src/test/java/com/hust310/SkillsMaster/BaiduApiTest.java
+++ src/test/java/com/hust310/SkillsMaster/BaiduApiTest.java
@@ -3,6 +3,7 @@ package com.hust310.SkillsMaster;
 import com.baidu.aip.contentcensor.AipContentCensor;
 import com.baidu.aip.contentcensor.EImgType;
 
+import com.hust310.SkillsMaster.config.BaiduAPI;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,14 +12,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 //@SpringBootTest
 public class BaiduApiTest {
 
-    public static final String APP_ID = "34889997";
-    public static final String API_KEY = "2od67QEHyfepWsXQvR0idplp";
-    public static final String SECRET_KEY = "KKdGfAqowXsyWgNdrGpnKNI8X2NfaFyj";
+//    public static final String APP_ID = "34889997";
+//    public static final String API_KEY = "2od67QEHyfepWsXQvR0idplp";
+//    public static final String SECRET_KEY = "KKdGfAqowXsyWgNdrGpnKNI8X2NfaFyj";
+
 
     @Test
     public void testText() {
 
-        AipContentCensor client=new AipContentCensor(APP_ID, API_KEY, SECRET_KEY);
+        AipContentCensor client=new AipContentCensor(BaiduAPI.APP_ID, BaiduAPI.API_KEY, BaiduAPI.SECRET_KEY);
         String text="蔡英文";
         JSONObject response = client.textCensorUserDefined(text);
         System.out.println(response.toString());
@@ -26,7 +28,7 @@ public class BaiduApiTest {
 
     @Test
     public void testImage() {
-        AipContentCensor client=new AipContentCensor(APP_ID, API_KEY, SECRET_KEY);
+        AipContentCensor client=new AipContentCensor(BaiduAPI.APP_ID, BaiduAPI.API_KEY, BaiduAPI.SECRET_KEY);
         String url="https://tse4-mm.cn.bing.net/th/id/OIP-C.kHinIF0bOKfFmAOxpibyfwAAAA?w=132&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7";
         JSONObject response = client.imageCensorUserDefined(url, EImgType.URL, null);
         System.out.println(response.toString());
